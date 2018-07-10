4ae824f95f9c78fccd392136b6e06b9d5e1788db
johao70 <perlazajohao@gmail.com>
2018-07-10 14:28:54 -0500

    cambios en home.html y home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,12 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notes = [
+    {id:1, title:'Nota 1', descripcion:'Descripcion Nota 1'}
+    {id:1, title:'Nota 2', descripcion:'Descripcion Nota 2'}
+    {id:1, title:'Nota 3', descripcion:'Descripcion Nota 3'}
 
+  ]
   constructor(public navCtrl: NavController) {
 
   }
