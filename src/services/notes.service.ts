e4bf865f397fb7eaa86cb858dc3be733caddab87
johao70 <perlazajohao@gmail.com>
2018-07-11 16:51:38 -0500

    cambios

--- src/services/notes.service.ts
+++ src/services/notes.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class NotesService{
     notes = [
-        {id:1, title:'Nota 1', description:'Descripcion Nota 1'},
+        {id:1, title:'Prueba 1', description:'Descripcion Nota 1'},
         {id:2, title:'Nota 2', description:'Descripcion Nota 2'},
-        {id:3, title:'Nota 3', description:'Descripcion Nota 3'}
+        {id:3, title:'Test 3', description:'Descripcion Nota 3'}
     ];
 
     public getNotes(){
